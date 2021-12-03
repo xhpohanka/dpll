aef226752c4caf0dbdf9ca65b7a2606d8274d5f3
Jan Pohanka <xhpohanka@gmail.com>
2021-12-03 09:42:36 +0100

    Use also bigger fs to prove function

--- dpll.c
+++ dpll.c
@@ -6,13 +6,20 @@
 #define USE_PSC
 
 #define F_PLL       32e6
-#define KL          0.1005f
-#define KI          5.0532e-03f
+
 #define PI          ((float) M_PI)
 #define F_NCO       55
 #define TIM_BITS    10
 #define ADC_MAX     (1 << 12)
 #define F_S         256
+
+#if F_S == 256
+# define KL          0.07853981633974483
+# define KI          0.003084251375340424 // fs = 256
+#elif F_S == 2560
+# define KI          3.0842513753404247e-05
+# define KL          0.007853981633974483 // fs = 2560
+#endif
 
 
 struct filter_desc {
@@ -24,8 +31,13 @@ struct filter_desc {
 
 // dolni propust butterworth, 4rad, 60Hz, fs = 256
 static struct filter_desc low_pass = {
+#if F_S == 256
     .b = (float []){ 0.07673978, 0.30695911, 0.46043867, 0.30695911, 0.07673978 },
     .a = (float []){ 1.0f, -0.2440988, 0.50486789, -0.05175419, 0.01882155 },
+#elif F_S == 2560
+    .b = (float []){ 2.44195653e-05, 9.76782612e-05, 1.46517392e-04, 9.76782612e-05, 2.44195653e-05 }, // fs = 2560
+    .a = (float []){ 1., -3.6153527 ,  4.91831572, -2.98287135,  0.68029904 },
+#endif
     .len = 5,
     .w = (float [5]) {}
 };
@@ -61,7 +73,7 @@ float dpll_step(int sample, int timval, int reload_val)
 
     // moving average of pe (ewma)
     // if small enough we can reduce action
-    float k1 = 0.01;
+    float k1 = 20.0 / F_S;
     avg = k1 * pe + (1 - k1) * avg;
 
     // bacha nechodi na vyssich fs
